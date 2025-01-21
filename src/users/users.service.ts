cd64aeeb6bb34b372cd3146bae40e07a2479c8d1
ttmatteus <matteusgn@gmail.com>
2025-01-21 16:11:01 -0300

    feat: add UsersService and UsersController
    - Implemented UsersService with methods to find users by ID, find by username, and create new users
    - Created UsersController with an endpoint to retrieve user data by ID

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { UserResponseDto } from 'src/auth/dto/user-response.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ){}
+
+    async findById(id: number): Promise<UserResponseDto> {
+        const user = await this.userRepository.findOne({ where: { id } });
+        if (!user) {
+            throw new NotFoundException('User not found');
+        }
+        return new UserResponseDto(user.id, user.username);
+    }
+
+    async findByUsername(username: string): Promise<User | null> {
+        return this.userRepository.findOne({ where: { username} });
+    }
+
+    async create(username: string, password: string): Promise<User> {
+        const newUser = this.userRepository.create({ username, password });
+        return this.userRepository.save(newUser);
+    }
+}
