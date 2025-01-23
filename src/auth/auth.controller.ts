2f961bfdb67a88f72b0745dbbb0eb485018b1710
ttmatteus <matteusgn@gmail.com>
2025-01-23 15:14:13 -0300

    Feat: add JwtAuthGuard e AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from 'src/users/user.entity';
+import { LoginUserDto } from './dto/login-user.dto.user';
+import { AuthTokenResponseDto } from './dto/auth-token-response.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
-export class AuthController {}
+@UseGuards(JwtAuthGuard)
+export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
+    @Post('register')
+    async register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+        return this.authService.register(registerUserDto);
+    }
+
+    @Post('login')
+    async login(@Body() loginUserDto: LoginUserDto): Promise<AuthTokenResponseDto> {
+        return this.authService.login(loginUserDto);
+    }
+}
